6a2f41d3564bb5053e35883a1306997a44f547fc
anjusena-wickramasinghe <anjalasenani@gmail.com>
2025-12-07 00:49:27 +0530

    sign-in user api route

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken"
+
 
 export const signup = async (req, res, next) => {
     const { name, email, password, profileImageUrl, adminJoinCode } = req.body;
@@ -46,4 +48,34 @@ export const signup = async (req, res, next) => {
     } catch (error) {
         return next(errorHandler(500, error.message));
     }
-};
+}
+
+export const signin = async (req,res,next)=>{
+    try{
+const{email,password}=req.body
+
+if(!email ||!password || email==="" ||password===""){
+    return next(errorHandler(400,"All fields are required"))
+}
+
+const ValidUser=await User.findOne({email})
+
+if(!ValidUser){
+    return next(errorHandler(404,"User not found"))
+}
+
+//compare password
+const validPassword = bcryptjs.compareSync(password,ValidUser.password)
+if(!validPassword){
+    return next(errorHandler(400,"Wrong credentials"))
+}
+
+const token = jwt.sign({id:ValidUser._id},process.env.JWT_SECRET)
+
+const{password:pass,...rest} = ValidUser._doc
+
+res.status(200).cookie("access_token",token,{httpOnly:true}).json(rest)
+    }catch(error){
+        next(error)
+    }
+}
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 
@@ -22,6 +23,8 @@ app.use(cors({
 }));
 
 app.use(express.json());
+
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
 
